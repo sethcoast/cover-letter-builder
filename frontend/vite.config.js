6b3f521dff624b039372da3c1dbdb4a1e5648a95
Seth Donaldson <sethd63@gmail.com>
2024-06-10 15:53:18 -0400

    updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,13 +6,28 @@ import path from 'path';
 
 dotenv.config();
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(process.env.SSL_KEY_FILE)),
-      cert: fs.readFileSync(path.resolve(process.env.SSL_CRT_FILE)),
-    }
-  },
-})
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     https: {
+//     }
+//   },
+// })
+export default defineConfig(({ mode }) => {
+  let serverConfig = {};
+  
+  if (mode === 'development') {
+    serverConfig = {
+      https: {
+        key: fs.readFileSync(path.resolve(process.env.SSL_KEY_FILE)),
+        cert: fs.readFileSync(path.resolve(process.env.SSL_CRT_FILE)),
+      }
+    };
+  }
+
+  return {
+    // other Vite configurations
+    server: serverConfig,
+  };
+});
