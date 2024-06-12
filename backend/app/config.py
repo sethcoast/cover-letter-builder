f2be58c85fd348a419ba054f70386371afbaaae0
Seth Donaldson <sethd63@gmail.com>
2024-06-12 12:35:36 -0400

    reformat config and update requirements.txt

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
-    SERPER_API_KEY = os.environ.get('SERPER_API_KEY')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    CELERY_BROKER_URL = os.environ.get('REDIS_URL')
-    CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')
-    REDIS_URL = os.environ.get('REDIS_URL')
+    OPENAI_API_KEY          = os.environ.get('OPENAI_API_KEY')
+    SERPER_API_KEY          = os.environ.get('SERPER_API_KEY')
+    SECRET_KEY              = os.environ.get('SECRET_KEY')
+    CELERY_BROKER_URL       = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
+    CELERY_RESULT_BACKEND   = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
+    REDIS_URL               = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
