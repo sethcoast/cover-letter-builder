03f7ebbd65668ae6d9dd7f6acbd5a46c8f744699
Seth Donaldson <sethd63@gmail.com>
2024-06-12 17:42:26 -0400

    reset head and restate redis url

--- backend/app/app.py
+++ backend/app/app.py
@@ -41,7 +41,7 @@ app.register_blueprint(bp)
 # Initialize the SocketIO app
 socketio = SocketIO(app,
                     cors_allowed_origins=["http://localhost:5173","https://cover-letter-builder-delta.vercel.app"],
-                    message_queue=Config.REDIS_URL,
+                    message_queue=os.getenv('REDIS_URL'),
                     async_mode='threading')
 
 # setup logger
